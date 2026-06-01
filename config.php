a5720b0960a77c7e08e891a819584ad4f3ec2575
sanjusalon20-wq <sanjusalon20@gmail.com>
2026-06-01 17:46:48 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("mysql.railway.internal", "railway", "EgDIZkeQudzkJpAgozxAarDQotyXvCPG", "railway");
+$conn = mysqli_connect("mysql.railway.internal", "root", "EgDIZkeQudzkJpAgozxAarDQotyXvCPG", "railway", 3306);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
